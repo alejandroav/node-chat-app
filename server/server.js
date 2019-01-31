bf015ac0c05fc1b9f67faf5a438adc1b4f096ea4
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-01-31 18:09:26 +0100

    Refactor, basic test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -17,25 +18,13 @@ io.on('connection', (socket) => {
 		console.log('User disconnected');
 	});
 
-	socket.emit('newMessage', {
-		from: 'admin',
-		text: 'Welcome to the chat',
-		createdAt: new Date().getTime()
-	});
+	socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat'));
 
-	socket.broadcast.emit('newMessage', {
-		from: 'admin',
-		text: 'New user joined',
-		createdAt: new Date().getTime()
-	});
+	socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
 
 	socket.on('createMessage', (message) => {
 		console.log(message);
-		io.emit('newMessage', {
-			from: message.from,
-			text: message.text,
-			createdAt: new Date().getTime()
-		});
+		io.emit('newMessage', generateMessage(message.from, message.text));
 	});
 });
 
