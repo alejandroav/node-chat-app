ff9732a8f92e91ccf57332d3458af72f15ccd4d3
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-01-31 17:43:29 +0100

    broadcast

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ io.on('connection', (socket) => {
 
 	socket.on('createMessage', (message) => {
 		console.log('Received message', message);
-		socket.emit('newMessage', {
+		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
