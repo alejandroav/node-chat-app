14f68c0e03c0c2f3c3bac7e0818838e2ec11c751
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-02-04 17:31:34 +0100

    moment.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,30 @@
 var socket = io();
 
 socket.on('newMessage', function(message) {
+	var formattedTime = moment(message.createdAt).format('HH:MM');
 	var li = $('<li></li>');
-	li.text(`${message.from}: ${message.text}`);
+	li.text(`${formattedTime} - ${message.from}: ${message.text}`);
 	$('#messages').append(li);
 });
 
 socket.on('newLocationMessage', function(message) {
+	var formattedTime = moment(message.createdAt).format('HH:MM');
 	var li = $('<li></li>');
-	var a = $('<a target="_blank">My current location</a>');
-	
+	var a = $('<a target="_blank">My current location</a>');	
 	a.attr('href', message.url);
-	li.text(`${message.from}: `);
+	li.text(`${formattedTime} - ${message.from}: `);
 	li.append(a);
 	$('#messages').append(li);
 });
 
+var messageTextBox = $('[name=message]');
 $('#message-form').on('submit', function(e) {
 	e.preventDefault();
-
 	socket.emit('createMessage', {
 		from: 'User',
 		text: $('[name=message]').val()
 	}, function() {
-
+		messageTextBox.val('');
 	});
 });
 
@@ -32,14 +33,16 @@ locationButton.on('click', function() {
 	if (!navigator.geolocation) {
 		return alert('Geolocation not supported by your browser.');
 	} else {
+		locationButton.attr('disabled', 'disabled').text('Sending location');
 		navigator.geolocation.getCurrentPosition(function(position) {
+			locationButton.removeAttr('disabled').text('Send location');
 			socket.emit('createLocationMessage', {
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude
 			});
 		}, function() {
+			locationButton.removeAttr('disabled').text('Send location');
 			alert('Unable to fetch location.');
 		});
 	}
 });
-
