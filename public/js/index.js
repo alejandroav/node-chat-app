7aef379aafbcf5568550aa0d55d607d7a4d28b55
Alejandro Alarcón Villena <alejandroalarcon93@gmail.com>
2019-02-04 16:37:52 +0100

    geolocation messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
 var socket = io();
 
-socket.on('disconnect', function() {
-	console.log('Disconnected from server');
-});
-
 socket.on('newMessage', function(message) {
-	console.log(message);
 	var li = $('<li></li>');
 	li.text(`${message.from}: ${message.text}`);
+	$('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function(message) {
+	var li = $('<li></li>');
+	var a = $('<a target="_blank">My current location</a>');
+	
+	a.attr('href', message.url);
+	li.text(`${message.from}: `);
+	li.append(a);
 	$('#messages').append(li);
 });
 
@@ -21,3 +26,20 @@ $('#message-form').on('submit', function(e) {
 
 	});
 });
+
+var locationButton = $('#send-location');
+locationButton.on('click', function() {
+	if (!navigator.geolocation) {
+		return alert('Geolocation not supported by your browser.');
+	} else {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			socket.emit('createLocationMessage', {
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			});
+		}, function() {
+			alert('Unable to fetch location.');
+		});
+	}
+});
+
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate correct message object', () => {
@@ -8,6 +8,19 @@ describe('generateMessage', () => {
 		var message = generateMessage(from, text);	
 
 		expect(message).toInclude({from, text});
+		expect(message.createdAt).toBeA('number');
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate correct location message', () => {
+		var from = 'user';
+		var lat = 123;
+		var lon = 123;
+		var url = 'https://google.com/maps?q=123,123';
+		var message = generateLocationMessage(from, lat, lon);
+
+		expect(message).toInclude({from, url});
 		expect(message.createdAt).toBeA('number');
 	});
 });
